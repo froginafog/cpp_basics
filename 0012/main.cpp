afcb1254da0d310cec805cafa1b0b31e731f980f
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-22 21:03:53 -0400

    Add files via upload

--- 0012/main.cpp
+++ 0012/main.cpp
@@ -12,7 +12,7 @@ int main()
 
     int quotient;  //create a variable of the type "int" and call it "quotient"
 
-    quotient = a / b;  //divide the value stored in "a" by the value stored in "b"
+    quotient = a / b;  //divide the value stored in "a" by the value stored in "b" and store the result in "quotient"
     //a / b = 7 / 2 = (1 + 6) / 2 = 1/2 + 6/2 = 0 + 3 = 3
     //an "int" divided by an "int" has no fractional part
 
