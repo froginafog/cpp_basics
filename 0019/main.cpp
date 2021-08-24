f40a1c6e144ab66f56d5528cfa171c1a4a0bae32
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-24 11:18:58 -0400

    Add files via upload

---
+++ 0019/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void print_message(string message)
+{
+    cout << message << endl;
+}
+
+int main()
+{
+    string message = "Hello Frog Bye Frog";
+
+    print_message(message);
+
+    return 0;
+}
+
+/*
+Hello Frog Bye Frog
+*/
