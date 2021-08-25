85b55df04475c9f33519bc35b019bb9780e6e559
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-25 09:18:11 -0400

    Update main.cpp

--- 0053/main.cpp
+++ 0053/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 	string array_of_strings[]{"Yesterday was warm.", "Today is cool.", "Tomorrow will be cold."};
-    int num_strings = sizeof(array_of_strings)/sizeof(array_of_strings[0]);
+    	int num_strings = sizeof(array_of_strings)/sizeof(array_of_strings[0]);
 
 	for(int i = 0; i < num_strings; i++)
 	{
