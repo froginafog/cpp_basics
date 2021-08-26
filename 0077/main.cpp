1b301e00718369f8ca57a5e94708c1a8333b0b42
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-26 17:48:47 -0400

    Add files via upload

---
+++ 0077/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int countdown(int n);
+
+int main()
+{
+    int starting_number = 5;
+    int ending_number;
+
+    cout << "starting_number: " << starting_number << endl;
+    ending_number = countdown(starting_number);
+    cout << "ending_number: " << ending_number << endl;
+
+    return 0;
+}
+
+int countdown(int n)
+{
+    cout << n << endl;
+    if(n == 0)
+    {
+        return n;
+    }
+    else
+    {
+        countdown(n - 1);
+    }
+}
+
+/*
+starting_number: 5
+5
+4
+3
+2
+1
+0
+ending_number: 0
+*/
