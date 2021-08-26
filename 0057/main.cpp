f0a2c8210ca5a2c61e935cbb64c6cb07ddf0fb63
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-25 22:42:35 -0400

    Add files via upload

---
+++ 0057/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int * increment(int);
+
+int main()
+{
+	int number;
+
+	number = 0;
+	number = *increment(number);
+	cout << "number: " << number << endl;
+
+	return 0;
+}
+
+int * increment(int n)
+{
+	int * p;
+
+	p = &n;
+	n++;
+
+	return p;
+}
+
+/*
+number: 1
+*/
---
+++ 0058/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int * increment(int);
+
+int main()
+{
+	int number;
+	int * pointer;
+
+	number = 0;
+	pointer = increment(number);
+	number = *pointer;
+	cout << "number: " << number << endl;
+
+	return 0;
+}
+
+int * increment(int n)
+{
+	int * p;
+
+	p = &n;
+	n++;
+
+	return p;
+}
+
+/*
+number: 1
+*/
