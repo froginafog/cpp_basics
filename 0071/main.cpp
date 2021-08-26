03e0c42c3a31c719282fdcf292e82fa0f2e82d67
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-26 11:45:56 -0400

    Add files via upload

---
+++ 0071/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+
+	a = 1;
+	b = 2;
+	cout << "a/b: " << (a/b) << endl;
+	cout << "(double)a/b: " << (double)a/b << endl;
+
+	return 0;
+}
+
+/*
+a/b: 0
+(double)a/b: 0.5
+*/
---
+++ 0072/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char choice;
+
+	cout << "Enter A, B, C, or D: ";
+	cin >> choice;
+
+	switch(choice)
+	{
+        case 'A':
+            cout << "Menu A:" << endl;
+            cout << "Hamburger, Fries, Drink" << endl;
+            break;
+        case 'B':
+            cout << "Menu B:" << endl;
+            cout << "Hot Dog, Fries, Drink" << endl;
+            break;
+        case 'C':
+            cout << "Menu C:" << endl;
+            cout << "Pizza, Fries, Drink" << endl;
+            break;
+        case 'D':
+            cout << "Menu D:" << endl;
+            cout << "Pita, Fries, Drink" << endl;
+            break;
+        default:
+            cout << "Invalid Choice" << endl;
+	}
+
+	return 0;
+}
+
+/*
+Enter A, B, C, or D: C
+Menu C:
+Pizza, Fries, Drink
+*/
