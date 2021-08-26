80c217e911aed0ad5d6314d14b85762b85fd6e3a
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-26 18:01:28 -0400

    Add files via upload

---
+++ 0078/main.cpp
@@ -0,0 +1,28 @@
+//n! = n * (n-1)! = n * (n-1) * (n-2) *... * 3 * 2 * 1
+#include <iostream>
+
+using namespace std;
+
+unsigned long long int factorial(unsigned long long int);
+
+int main()
+{
+	int number;
+
+	number = 4;
+	cout << number << "!: " << factorial(number) << endl;
+
+	return 0;
+}
+
+unsigned long long int factorial(unsigned long long int n)
+{
+	if(n == 0 || n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return n * factorial(n-1);
+	}
+}
