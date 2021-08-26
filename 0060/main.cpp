44c26beba580c9e4261f5fd649550f8898731fa3
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-25 23:06:36 -0400

    Add files via upload

---
+++ 0060/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int * p_1 = new int;
+	int * p_2 = new int;
+	int * result = new int;
+
+	*p_1 = 5;
+	*p_2 = 10;
+	*result = *p_1 + *p_2;
+	cout << "*result: " << *result << endl;
+
+	return 0;
+}
+
+/*
+*result: 15
+*/
