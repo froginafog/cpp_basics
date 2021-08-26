b0720f20839421bb95f2ea9d106c85b9585b2a94
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-25 23:06:29 -0400

    Add files via upload

---
+++ 0059/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int * pointer = new int;
+
+	*pointer = 10;
+	cout << "*pointer: " << *pointer << endl;
+
+	return 0;
+}
+
+/*
+*pointer: 10
+*/
