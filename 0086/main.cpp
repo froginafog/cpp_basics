4b349cbf58f8d42904596fa73382619387a0ddab
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-27 14:56:23 -0400

    Add files via upload

---
+++ 0086/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+//create a class called "Account"
+class Account
+{
+public:
+    string name;
+	int ID;
+private:
+    double account_balance;
+};
+
+int main()
+{
+    Account account_1;  //create an object called "account_1" of the class called "Account"
+    account_1.name = "Bob The Great";
+    account_1.ID = 12345;
+    //account_1.account_balance = 123.45;
+    //We cannot access "account_balance" because "account_balance" is a private member of the class Account.
+
+    Account account_2;  //create an object called "account_2" of the class called "Account"
+    account_2.name = "Robin The Hood";
+    account_2.ID = 23456;
+    //account_2.account_balance = 234.56;
+    //We cannot access "account_balance" because "account_balance" is a private member of the class Account.
+
+    cout << "account_1.name: " << account_1.name << endl;
+    cout << "account_1.ID: " << account_1.ID << endl;
+    cout << "----------------------------------" << endl;
+    cout << "account_2.name: " << account_2.name << endl;
+    cout << "account_2.ID: " << account_2.ID << endl;
+
+	return 0;
+}
+
+/*
+account_1.name: Bob The Great
+account_1.ID: 12345
+----------------------------------
+account_2.name: Robin The Hood
+account_2.ID: 23456
+ */
---
+++ 0087/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Account
+{
+private:
+	string name;
+	int ID;
+
+public:
+	void setName(string inputName)
+	{
+		name = inputName;
+	}
+
+	string getName()
+	{
+		return name;
+	}
+
+	void setID(int inputID)
+	{
+		ID = inputID;
+	}
+
+	int getID()
+	{
+		return ID;
+	}
+
+	//We can use the public functions of the class to access the private members of the class.
+};
+
+int main()
+{
+	//create an object of the class "Account"
+	//"account1" is the name of the object
+	Account account_1;
+
+	//create an object of the class "Account"
+	//"account2" is the name of the object
+	Account account_2;
+
+	account_1.setName("Bob The Great");
+	account_1.setID(123);
+
+	account_2.setName("Robin The Hood");
+	account_2.setID(456);
+
+	cout << "account_1.getName(): " << account_1.getName() << endl;
+	cout << "account_1.getID()  : " << account_1.getID() << endl;
+    cout << "----------------------------------------" << endl;
+	cout << "account_2.getName(): " << account_2.getName() << endl;
+	cout << "account_2.getID()  : " << account_2.getID() << endl;
+
+	return 0;
+}
+
+/*
+account_1.getName(): Bob The Great
+account_1.getID()  : 123
+----------------------------------------
+account_2.getName(): Robin The Hood
+account_2.getID()  : 456
+*/
