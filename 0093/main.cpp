d9a89c774ea12ed824961c84085a8ea3cbb6f5f8
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-27 16:35:45 -0400

    Add files via upload

---
+++ 0093/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>  //to use ofstream
+
+using namespace std;
+
+int main()
+{
+	int number;  //account number
+	string name; //account name
+	double balance; //account balance
+	int numberOfAccounts;
+
+	ofstream outputToFile("account.txt", ios::out);  //ofstream constructor
+	                                               //opens or creates a file "accounts.txt"
+
+	if(!outputToFile)  //if we failed to create the file
+	{
+		cerr << "failed to open the file" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+    cout << "Enter account number, account name, and account balance, each separated by a space: ";
+    cin >> number >> name >> balance;
+    outputToFile << "account number : " << number << endl
+                 << "account name   : " << name << endl
+                 << "account balance: " << balance << endl
+                 << "--------------------------------------" << endl;
+
+	return 0;
+}
+
+
