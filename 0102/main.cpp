b70510dafc8fa72d67c7cc5504871934375cfd76
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-27 21:55:19 -0400

    Add files via upload

---
+++ 0102/main.cpp
@@ -0,0 +1,42 @@
+//overloading
+//Functions with the same name with different types of variable in the parameters
+//can be called based on the types of variables in the parameters.
+#include <iostream>
+
+using namespace std;
+
+void print(char);
+void print(int);
+void print(double);
+
+int main()
+{
+	print('T');
+	print(5);
+	print(123.45);
+
+	return 0;
+}
+
+void print(char inputCharacter)
+{
+	cout << "character: " << inputCharacter << endl;
+}
+
+void print(int inputInteger)
+{
+	cout << "integer: " << inputInteger << endl;
+}
+
+void print(double inputDouble)
+{
+	cout << "double: " << inputDouble << endl;
+}
+
+/*
+character: T
+integer: 5
+double: 123.45
+ */
+
+
