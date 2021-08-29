0902ec87447a5368aa658a486da4bdc3ea6fcb75
Liang D.S <80227533+froginafog@users.noreply.github.com>
2021-08-29 07:59:50 -0400

    Add files via upload

---
+++ 0103/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>  //to use vector<int>
+
+using namespace std;
+
+int main()
+{
+	vector<int> numbers;
+
+	numbers.push_back(1);
+	numbers.push_back(2);
+	numbers.push_back(3);
+	numbers.push_back(4);
+	numbers.push_back(5);
+
+	size_t num_elements = numbers.size();
+	cout << "num_elements: " << num_elements << endl;
+
+	cout << "numbers: ";
+	for(auto it = numbers.begin(); it != numbers.end(); it++)  //it = iterator
+	{
+		cout << *it << ' ';
+	}
+	cout << endl;
+
+	cout << "-------------------------------------------" << endl;
+
+	cout << "clear the vector" << endl;
+	numbers.clear();  //remove all elements from the vector
+
+	num_elements = numbers.size();
+	cout << "num_elements: " << num_elements << endl;
+
+	cout << "numbers: ";
+	for(auto it = numbers.begin(); it != numbers.end(); it++)  //it = iterator
+	{
+		cout << *it << ' ';
+	}
+	cout << endl;
+
+	//By using iterators, we can see that the vector has been cleared.
+
+	return 0;
+}
+
+/*
+num_elements: 5
+numbers: 1 2 3 4 5
+-------------------------------------------
+clear the vector
+num_elements: 0
+numbers:
+ */
---
+++ 0104/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> numbers(5);  //Create a vector of 5 elements of the type int.
+
+	cout << "number of elements: " << numbers.size() << endl;
+
+	numbers[0] = 1;
+	numbers[1] = 2;
+	numbers[2] = 3;
+	numbers[3] = 4;
+	numbers[4] = 5;
+
+	cout << "numbers: ";
+	for(auto it = numbers.begin(); it != numbers.end(); it++)  //it = iterator
+	{
+		cout << *it << ' ';
+	}
+	cout << endl;
+
+	cout << "erase the vector" << endl;
+	numbers.erase(numbers.begin(), numbers.end());
+	//numbers.begin() is of the type "iterator"
+
+	cout << "numbers: ";
+	for(auto it = numbers.begin(); it != numbers.end(); it++)  //it = iterator
+	{
+		cout << *it << ' ';
+	}
+	cout << endl;
+
+	return 0;
+}
+
+/*
+number of elements: 5
+numbers: 1 2 3 4 5
+erase the vector
+numbers:
+ */
